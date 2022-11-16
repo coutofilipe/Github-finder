f27757561ca11fac889dfa9a9ba31bca37df2347
coutofilipe <32967734+coutofilipe@users.noreply.github.com>
2022-11-16 16:19:08 +0000

    updated gitignore to prevent burning api token on commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,12 @@
+import UserResults from "../components/users/UserResults"
+
 function Home() {
   return (
     <div>
-      <h1 className="text-6xl">Welcome</h1>
-      {process.env.REACT_APP_GITHUB_TOKEN}
+      <>
+        {/* Search component goes here */}
+        <UserResults />
+      </>
     </div>
 
   )
